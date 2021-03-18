680e8ecf120d78b3aab877641347c9a3dc7d8c4e
mYahagi <myahagi@hey.jp>
2021-03-18 21:45:52 +0900

    各出力パターン用のメソッド作成

--- main.go
+++ main.go
@@ -14,12 +14,51 @@ func main() {
 		return
 	}
 
-	fmt.Println("F1: " + factors[0] + " 親: " + factors[1] + " + " + factors[2])
+	fmt.Println(factors[0])
+	printFather(factors[1])
+	printMother(factors[2])
+
 	fmt.Println("----- 以降ここから繰り返し -----")
-	fmt.Println("F1: " + factors[3] + " 親: F1 " + factors[0] + " + " + factors[1])
-	fmt.Println("F1: " + factors[2] + " 親: F1 " + factors[0] + " + " + "F1 " + factors[3])
-	fmt.Println("F1: " + factors[1] + " 親: F1 " + factors[2] + " + " + "F1 " + factors[3])
-	fmt.Println("F2: " + factors[0] + " 親: F1 " + factors[2] + " + " + "F1 " + factors[3])
+
+	fmt.Println(factors[3])
+	printFather(factors[0])
+	printFatherAncestors(factors[1], factors[2])
+	printMother(factors[2])
+
+	fmt.Println(factors[2])
+	printFather(factors[0])
+	printFatherAncestors(factors[1], factors[2])
+	printMother(factors[3])
+	printMotherAncestors(factors[0], factors[2])
+
+	fmt.Println(factors[1])
+	printFather(factors[2])
+	printFatherAncestors(factors[0], factors[3])
+	printMother(factors[3])
+	printMotherAncestors(factors[0], factors[2])
+
+	fmt.Println(factors[0])
+	printFather(factors[1])
+	printFatherAncestors(factors[2], factors[3])
+	printMother(factors[2])
+	printMotherAncestors(factors[0], factors[3])
 	fmt.Println("----- ここまで繰り返し -----")
-	fmt.Println("F2: " + factors[3] + " 親: F2 " + factors[0] + " + " + "F1 " + factors[1])
+}
+
+func printFather(father string) {
+	fmt.Println("   ├─  " + father)
+}
+
+func printFatherAncestors(grantFather, grantMother string) {
+	fmt.Println("   │         ├─" + grantFather)
+	fmt.Println("   │         └─" + grantMother)
+}
+
+func printMother(mother string) {
+	fmt.Println("   └─  " + mother)
+}
+
+func printMotherAncestors(grantFather, grantMother string) {
+	fmt.Println("             ├─" + grantFather)
+	fmt.Println("             └─" + grantMother)
 }
