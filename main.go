bfea8fe1526f0477b8e48432845ee31f35b9c65f
mYahagi <myahagi@hey.jp>
2021-03-18 20:24:04 +0900

    一通り愚直に書いた

--- main.go
+++ main.go
@@ -14,9 +14,12 @@ func main() {
 		return
 	}
 
-	fmt.Println("----- 第一世代 -----")
-	fmt.Println(factors[0] + " 親: " + factors[1] + " + " + factors[2])
-	//for _, v := range factors {
-	//	fmt.Println(v)
-	//}
+	fmt.Println("F1: " + factors[0] + " 親: " + factors[1] + " + " + factors[2])
+	fmt.Println("----- 以降ここから繰り返し -----")
+	fmt.Println("F1: " + factors[3] + " 親: F1 " + factors[0] + " + " + factors[1])
+	fmt.Println("F1: " + factors[2] + " 親: F1 " + factors[0] + " + " + "F1 " + factors[3])
+	fmt.Println("F1: " + factors[1] + " 親: F1 " + factors[2] + " + " + "F1 " + factors[3])
+	fmt.Println("F2: " + factors[0] + " 親: F1 " + factors[2] + " + " + "F1 " + factors[3])
+	fmt.Println("----- ここまで繰り返し -----")
+	fmt.Println("F2: " + factors[3] + " 親: F2 " + factors[0] + " + " + "F1 " + factors[1])
 }
